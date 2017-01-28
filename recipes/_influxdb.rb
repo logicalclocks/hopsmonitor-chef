8c8d6c98b901d9b0a84939135e9ff9daed52c4c0
Jim Dowling <jdowling@sics.se>
2017-01-28 03:23:36 +0100

    hotfix

--- recipes/_influxdb.rb
+++ recipes/_influxdb.rb
@@ -71,123 +71,14 @@ end
 
 case node.platform_family
   when "debian"
-    package "ruby-dev" do
-      action :install
-    end
+    package "ruby-dev"
   when "rhel"
-    package "ruby-devel" do
-      action :install
-    end
-  end
+    package "ruby-devel" 
 end
 
 include_recipe 'influxdb::ruby_client'
 
 dbname = 'graphite'
-
-# node.override['influxdb']['config'] =
-# {
-#     'bind-address' => "#{my_private_ip}:#{node.influxdb.port}",
-#     "meta" => {
-#         "dir" => "#{node.influxdb.base_dir}/meta",
-#         'retention-autocreate' => true,
-#         'logging-enabled' => true
-#     },
-#     "data" => {
-#         "dir" => "#{node.influxdb.base_dir}/data",
-#         "wal-dir" => "#{node.influxdb.base_dir}/wal",
-#         'engine' => 'tsm1',
-#         'wal-logging-enabled' => true,
-#         'query-log-enabled' => true,
-#         'cache-max-memory-size' => 524_288_000,
-#         'cache-snapshot-memory-size' => 26_214_400,
-#         'cache-snapshot-write-cold-duration' => '1h0m0s',
-#         'compact-full-write-cold-duration' => '24h0m0s',
-#         'max-points-per-block' => 0,
-#         'trace-logging-enabled' => false
-#     },
-#     'coordinator' => {
-#       'write-timeout' => '10s',
-#       'max-concurrent-queries' => 0,
-#       'query-timeout' => '0',
-#       'log-queries-after' => '0',
-#       'max-select-point' => 0,
-#       'max-select-series' => 0,
-#       'max-select-buckets' => 0
-#     }, 
-#    'retention' => {
-#       'enabled' => true,
-#       'check-interval' => '30m0s'
-#     },
-#    'shard-precreation' => {
-#       'enabled' => true,
-#       'check-interval' => '10m0s',
-#       'advance-period' => '30m0s'
-#     },
-#     'admin' => {
-#         'enabled' => true,
-#         'bind-address' => ':8083',
-#         'https-enabled' => false,
-#         'https-certificate' => node['influxdb']['ssl_cert_file_path'],
-#     },
-#       'monitor' => {
-#         'store-enabled' => true,
-#         'store-database' => '_internal',
-#         'store-interval' => '10s',
-#       },
-#       'subscriber' => {
-#         'enabled' => true,
-#         'http-timeout' => '30s',
-#       },
-#       'http' => {
-#         'enabled' => true,
-#         'bind-address' => ':8086',
-#         'auth-enabled' => false,
-#         'log-enabled' => true,
-#         'write-tracing' => false,
-#         'pprof-enabled' => false,
-#         'https-enabled' => false,
-#         'https-certificate' => node['influxdb']['ssl_cert_file_path'],
-#         'https-private-key' => '',
-#         'max-row-limt' => 10_000,
-#         'max-connection-limit' => 0,
-#         'shared-secret' => '',
-#         'realm' => 'InfluDB',
-#       }, 
-#   'graphite' => [{
-#         'enabled' => true,
-#         'bind-address' => "#{my_private_ip}:2003",
-#         'conprotocol' => "tcp",
-#     }],
-#   'continuous_queries' => {
-#     'log-enabled' => true,
-#     'enabled' => true,
-#     'run-interval' => '1s',
-#   }
-
-
-# }
-
-
-# influxdb_install 'influxdb' do
-#   arch_type 'amd64' # if undefined will auto detect
-#   include_repository true # default
-#   influxdb_key 'https://repos.influxdata.com/influxdb.key' # default
-#   action :install # default
-# end
-
-# service 'influxdb' do
-#   action :nothing
-# end
-
-
-# influxdb_config node['influxdb']['config_file_path'] do
-#   path node.influxdb.base_dir + "/conf"
-#   config node['influxdb']['config']
-#   action :create
-#   notifies :restart, 'service[influxdb]'
-# end
-
 
 # Create a test database
 influxdb_database dbname do

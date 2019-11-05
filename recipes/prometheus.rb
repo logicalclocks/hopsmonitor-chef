5655dcbbf1c2d911a703710b653649d4d4da3eee
Fabio Buso <buso.fabio@gmail.com>
2019-11-05 11:04:04 +0100

    [HOPSWORKS-1404] Prometheus should handle the case in which recipes are missing.

--- recipes/prometheus.rb
+++ recipes/prometheus.rb
@@ -41,71 +41,111 @@ link node['prometheus']['base_dir'] do
   to node['prometheus']['home']
 end
 
-alertmanagers = private_recipe_ips("hopsmonitor", "alertmanager")
-alertmanagers = alertmanagers.map{ |alertmanager| 
-  Resolv.getname(alertmanager) + ":" + node['alertmanager']['port'] 
-}
-
-node_exporters = private_recipe_ips("hopsmonitor", "node_exporter")
-node_exporters = node_exporters.map{ |node_exporter| 
-  Resolv.getname(node_exporter) + ":" + node['node_exporter']['port'] 
-}
-
-mysqld_exporters = private_recipe_ips("ndb", "mysqld")
-mysqld_exporters = mysqld_exporters.map{ |mysqld_exporter| 
-  Resolv.getname(mysqld_exporter) + ":" + node['ndb']['mysqld']['metrics_port']
-}
-
-hops_exporters = []
-['nn', 'dn', 'rm', 'nm'].each do |service| 
-    hops_exporters << private_recipe_ips("hops", service).map{ |exporter| 
-        Resolv.getname(exporter) + ":" + node['hops'][service]['metrics_port']
-    }
+begin
+  alertmanagers = private_recipe_ips("hopsmonitor", "alertmanager")
+  alertmanagers = alertmanagers.map{ |alertmanager| 
+    Resolv.getname(alertmanager) + ":" + node['alertmanager']['port'] 
+  }
+rescue 
+  alertmanagers = []
 end
 
-kafka_exporters = private_recipe_ips("kkafka", "default")
-kafka_exporters = kafka_exporters.map{ |kafka_exporter| 
-  Resolv.getname(kafka_exporter) + ":" + node['kkafka']['metrics_port'] 
-}
-
-elastic_exporters = private_recipe_ips("elastic", "default")
-elastic_exporters = elastic_exporters.map{ |elastic_exporter| 
-  Resolv.getname(elastic_exporter) + ":" + node['elastic']['exporter']['port']
-}
-
-hive_exporters_tmp = private_recipe_ips("hive2", "default")
-# This is a bit of a wild assumption. "hive2::default" actually call 
-# both server2 and metastore recipes. This means it deploys 2 services.
-# As such, we duplicate the elements in the array. This ofc is going to break
-# if we start using the specific recipes (server2.rb and metastore.rb) instead of 
-# the default.rb. The proper fix is to use a service discovery. 
-hive_exporters_tmp = hive_exporters_tmp * 2 
-hive_exporters = []
-i = 0
-while i < hive_exporters_tmp.length
-  hive_exporters << Resolv.getname(hive_exporters_tmp[i]) + ":" + node['hive2']['hs2']['metrics_port'] 
-  hive_exporters << Resolv.getname(hive_exporters_tmp[i+1]) + ":" + node['hive2']['hm']['metrics_port'] 
-  i += 2
+begin
+  node_exporters = private_recipe_ips("hopsmonitor", "node_exporter")
+  node_exporters = node_exporters.map{ |node_exporter| 
+    Resolv.getname(node_exporter) + ":" + node['node_exporter']['port'] 
+  }
+rescue
+  node_exporters = []
 end
 
-airflow_exporters = private_recipe_ips("hops_airflow", "default")
-airflow_exporters = airflow_exporters.map{ |airflow_exporter| 
-  Resolv.getname(airflow_exporter) + ":" + node['airflow']["config"]["webserver"]["web_server_port"].to_s
-}
-
-hopsworks_port = "8181"
-if node.attribute?("hopsworks") && node['hopsworks'].attribute?("https") && node['hopsworks']['https'].attribute?("port")
-  hopsworks_port = node['hopsworks']['https']['port'].to_s
+begin
+  mysqld_exporters = private_recipe_ips("ndb", "mysqld")
+  mysqld_exporters = mysqld_exporters.map{ |mysqld_exporter| 
+    Resolv.getname(mysqld_exporter) + ":" + node['ndb']['mysqld']['metrics_port']
+  }
+rescue
+  mysqld_exporters = []
 end
-hopsworks_exporters = private_recipe_ips("hopsworks", "default")
-hopsworks_exporters = hopsworks_exporters.map{ |hopsworks_exporter| 
-  Resolv.getname(hopsworks_exporter) + ":" + hopsworks_port
-}
 
-epipe_exporters = private_recipe_ips("epipe", "default")
-epipe_exporters = epipe_exporters.map{ |epipe_exporter| 
-  Resolv.getname(epipe_exporter) + ":" + node['epipe']["metrics_port"].to_s
-}
+begin
+  hops_exporters = []
+  ['nn', 'dn', 'rm', 'nm'].each do |service| 
+      hops_exporters << private_recipe_ips("hops", service).map{ |exporter| 
+          Resolv.getname(exporter) + ":" + node['hops'][service]['metrics_port']
+      }
+  end
+rescue
+  hops_exporters = []
+end
+
+begin
+  kafka_exporters = private_recipe_ips("kkafka", "default")
+  kafka_exporters = kafka_exporters.map{ |kafka_exporter| 
+    Resolv.getname(kafka_exporter) + ":" + node['kkafka']['metrics_port'] 
+  }
+rescue
+  kafka_exporters = []
+end
+
+begin
+  elastic_exporters = private_recipe_ips("elastic", "default")
+  elastic_exporters = elastic_exporters.map{ |elastic_exporter| 
+    Resolv.getname(elastic_exporter) + ":" + node['elastic']['exporter']['port']
+  }
+rescue
+  elastic_exporters = []
+end
+
+begin
+  hive_exporters_tmp = private_recipe_ips("hive2", "default")
+  # This is a bit of a wild assumption. "hive2::default" actually call 
+  # both server2 and metastore recipes. This means it deploys 2 services.
+  # As such, we duplicate the elements in the array. This ofc is going to break
+  # if we start using the specific recipes (server2.rb and metastore.rb) instead of 
+  # the default.rb. The proper fix is to use a service discovery. 
+  hive_exporters_tmp = hive_exporters_tmp * 2 
+  hive_exporters = []
+  i = 0
+  while i < hive_exporters_tmp.length
+    hive_exporters << Resolv.getname(hive_exporters_tmp[i]) + ":" + node['hive2']['hs2']['metrics_port'] 
+    hive_exporters << Resolv.getname(hive_exporters_tmp[i+1]) + ":" + node['hive2']['hm']['metrics_port'] 
+    i += 2
+  end
+rescue 
+  hive_exporters = []
+end
+
+begin 
+  airflow_exporters = private_recipe_ips("hops_airflow", "default")
+  airflow_exporters = airflow_exporters.map{ |airflow_exporter| 
+    Resolv.getname(airflow_exporter) + ":" + node['airflow']["config"]["webserver"]["web_server_port"].to_s
+  }
+rescue 
+  airflow_exporters = []
+end
+
+begin
+  hopsworks_port = "8181"
+  if node.attribute?("hopsworks") && node['hopsworks'].attribute?("https") && node['hopsworks']['https'].attribute?("port")
+    hopsworks_port = node['hopsworks']['https']['port'].to_s
+  end
+  hopsworks_exporters = private_recipe_ips("hopsworks", "default")
+  hopsworks_exporters = hopsworks_exporters.map{ |hopsworks_exporter| 
+    Resolv.getname(hopsworks_exporter) + ":" + hopsworks_port
+  }
+rescue
+  hopsworks_exporters = []
+end
+
+begin
+  epipe_exporters = private_recipe_ips("epipe", "default")
+  epipe_exporters = epipe_exporters.map{ |epipe_exporter| 
+    Resolv.getname(epipe_exporter) + ":" + node['epipe']["metrics_port"].to_s
+  }
+rescue
+  epipe_exporters = []
+end
 
 template "#{node['prometheus']['base_dir']}/prometheus.yml" do
   source "prometheus.yml.erb" 

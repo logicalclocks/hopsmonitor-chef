f884ac8d24cecb53de649abcf412cbffc36fab3a
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-07-22 08:11:38 +0200

    [HOPSWORKS-1657] hopsmon user certificate for influxdb (#57)

--- recipes/prometheus.rb
+++ recipes/prometheus.rb
@@ -1,21 +1,6 @@
 require 'resolv'
 
-# Prometheus can run before consul, so we need the alternative URL for Hopsworks
-hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:8181" 
-if node.attribute? "hopsworks"
-  if node["hopsworks"].attribute? "https" and node["hopsworks"]['https'].attribute? ('port')
-    hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:#{node['hopsworks']['https']['port']}"
-  end
-end
-
-crypto_dir = x509_helper.get_crypto_dir(node['hopsmonitor']['user'])
-kagent_hopsify "Generate x.509" do
-  user node['hopsmonitor']['user']
-  crypto_directory crypto_dir
-  hopsworks_alt_url hopsworks_alt_url
-  action :generate_x509
-  not_if { conda_helpers.is_upgrade || node["kagent"]["test"] == true }
-end
+include_recipe "hopsmonitor::_security"
 
 #
 # Prometheus installation
@@ -58,6 +43,7 @@ link node['prometheus']['base_dir'] do
   to node['prometheus']['home']
 end
 
+crypto_dir = x509_helper.get_crypto_dir(node['hopsmonitor']['user'])
 certificate = "#{crypto_dir}/#{x509_helper.get_certificate_bundle_name(node['hopsmonitor']['user'])}"
 key = "#{crypto_dir}/#{x509_helper.get_private_key_pkcs8_name(node['hopsmonitor']['user'])}"
 hops_ca = "#{crypto_dir}/#{x509_helper.get_hops_ca_bundle_name()}"

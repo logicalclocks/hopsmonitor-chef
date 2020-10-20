f6a48a6c5269c54a5d2693f13a61174eafbc1835
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-10-20 09:22:20 +0200

    [HOPSWORKS-2094] Setup gpu monitoring even when gpu is not available but not start it (#63)

--- recipes/node_exporter.rb
+++ recipes/node_exporter.rb
@@ -74,48 +74,48 @@ if node['kagent']['enabled'] == "true"
    end
 end
 
-# If the machine has GPUs, configure the nvml_monitor.py
+cookbook_file "#{node['node_exporter']['base_dir']}/nvml_monitor.py" do
+  source 'nvml_monitor.py'
+  owner node['hopsmonitor']['user']
+  group node['hopsmonitor']['group']
+  mode '0700'
+  action :create
+end
+
+directory node['node_exporter']['text_metrics'] do
+  owner node['hopsmonitor']['user']
+  group node['hopsmonitor']['group']
+  mode '0700'
+  action :create
+end
+
+case node['platform_family']
+when "rhel"
+  systemd_script = "/usr/lib/systemd/system/nvml_monitor.service" 
+else
+  systemd_script = "/lib/systemd/system/nvml_monitor.service"
+end
+  
+template systemd_script do
+  source "nvml_monitor.service.erb"
+  owner "root"
+  group "root"
+  mode 0664
+end
+
+# If the machine has GPUs, start systemd unit
 if node['cuda']['accept_nvidia_download_terms'].eql?("true")
-
-  cookbook_file "#{node['node_exporter']['base_dir']}/nvml_monitor.py" do
-    source 'nvml_monitor.py'
-    owner node['hopsmonitor']['user']
-    group node['hopsmonitor']['group']
-    mode '0700'
-    action :create
-  end
-
-  directory node['node_exporter']['text_metrics'] do
-    owner node['hopsmonitor']['user']
-    group node['hopsmonitor']['group']
-    mode '0700'
-    action :create
-  end
-
-  case node['platform_family']
-  when "rhel"
-    systemd_script = "/usr/lib/systemd/system/nvml_monitor.service" 
-  else
-    systemd_script = "/lib/systemd/system/nvml_monitor.service"
-  end
   
-  service "nvml_monitor" do
-    provider Chef::Provider::Service::Systemd
-    supports :restart => true, :stop => true, :start => true, :status => true
-    action :nothing
+  systemd_unit "nvml_monitor.service" do
+    action [:start]
   end
-  
-  template systemd_script do
-    source "nvml_monitor.service.erb"
-    owner "root"
-    group "root"
-    mode 0664
-    if node['services']['enabled'] == "true"
-      notifies :enable, "service[nvml_monitor]"
+
+  if node['services']['enabled'].casecmp?("true")
+    systemd_unit "nvml_monitor.service" do
+      action :enable
     end
-    notifies :restart, "service[nvml_monitor]"
   end
-  
+
   kagent_config "nvml_monitor" do
     action :systemd_reload
   end

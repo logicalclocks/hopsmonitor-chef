eda4e8332710730814216e68108fa8906b255eb6
Fabio Buso <fabio@hopsworks.ai>
2023-11-30 01:23:26 +0100

    [HWORKS-861] Disable Kafka alerting rules when bring your own kafka is enabled

--- recipes/prometheus.rb
+++ recipes/prometheus.rb
@@ -180,7 +180,6 @@ rules = [
   "hive",
   "consul",
   "hopsworks",
-  "kafka",
   "onlinefs",
   "opensearch",
   "db",
@@ -188,7 +187,14 @@ rules = [
   "yarn",
   "host",
   "rdrs"
-].each { |rule_file|
+]
+
+# Add the kafka rules if bring your own kafka is not enabled
+if node.attribute?('hopsworks') and node['hopsworks']['enable_bring_your_own_kafka'].casecmp?("false")
+  rules |= ['kafka']
+end
+
+rules.each { |rule_file|
   template "#{node['prometheus']['rules_dir']}/#{rule_file}.rules.yml" do
     source "rules/#{rule_file}.rules.yml.erb"
     owner node['hopsmonitor']['user']
